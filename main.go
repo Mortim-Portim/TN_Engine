966ea54c3762b92cf5766c50a9e3685e003f6eeb
BuildTools <unconfigured@null.spigotmc.org>
2020-11-17 13:35:22 +0100

    Update

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
-package main 
+package main
 
 import (
-	"marvin/TN_Engine/TNE"
 	"marvin/GraphEng/GE"
+	"marvin/TN_Engine/TNE"
+
 	//"runtime"
-	"github.com/hajimehoshi/ebiten"
 	"fmt"
 	"time"
+
+	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 var (
 	screenWidth  = 1600
 	screenHeight = 900
-	FPS = 30
+	FPS          = 30
 )
+
 func StartGame(g ebiten.Game) {
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("TN_Engine Test")
@@ -27,16 +30,17 @@ func StartGame(g ebiten.Game) {
 	GE.ShitImDying(err)
 }
 
-
 type TestGame struct {
 	character *TNE.Player
 	world     *TNE.World
-	rec  *GE.Recorder
-	frame int
+	rec       *GE.Recorder
+	frame     int
 }
+
 func (g *TestGame) Init(screen *ebiten.Image) {}
 
 var timeTaken int64
+
 func (g *TestGame) Update(screen *ebiten.Image) error {
 	startTime := time.Now()
 	moving := false
@@ -69,27 +73,27 @@ func (g *TestGame) Update(screen *ebiten.Image) error {
 		moving = true
 	}
 	g.character.KeepMoving(moving)
-	
+
 	//Update chunks
 	g.world.UpdatePlayerChunks(g.world.Players)
-	
+
 	//Update world
 	g.world.UpdateActivePlayer()
 	g.world.UpdateDrawables()
 	g.world.UpdateWorldStructure()
 	g.world.Draw(screen)
-	
+
 	//Save video
 	if ebiten.IsKeyPressed(ebiten.KeyC) && !g.rec.IsSaving() {
 		g.rec.Save("./res/out")
 	}
 	g.rec.NextFrame(screen)
-	
-	g.frame ++
+
+	g.frame++
 	timeTaken = time.Now().Sub(startTime).Milliseconds()
 	msg := TNE.PrintPerformance(int(g.frame-1), int(timeTaken))
 	ebitenutil.DebugPrint(screen, msg)
-	GE.LogToFile(msg+"\n")
+	GE.LogToFile(msg + "\n")
 	fmt.Println(msg)
 	return nil
 }
@@ -97,52 +101,51 @@ func (g *TestGame) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
 }
 func main() {
-	screenWidth,screenHeight = ebiten.ScreenSizeInFullscreen()
+	screenWidth, screenHeight = ebiten.ScreenSizeInFullscreen()
 	GE.Init("")
 	GE.SetLogFile("./res/log.txt")
-	time.Sleep(time.Second*2)
-	
+	time.Sleep(time.Second * 2)
+
 	diceStart := time.Now()
 	result := TNE.RollDice(100000, 20)
 	fmt.Println("Rolling Dice took: ", time.Now().Sub(diceStart))
 	fmt.Println(result)
-	
+
 	game := &TestGame{nil, nil, GE.GetNewRecorder(FPS*5, 360, 202, FPS), 0}
-	
+
 	cf, err := TNE.GetEntityFactory("./res/creatures/", &game.frame, 3)
 	GE.ShitImDying(err)
-	
+
 	uFncs := make(map[string]func(e *TNE.Entity, world *TNE.World))
-	uFncs["test"] = func(ent *TNE.Entity, world *TNE.World){
+	uFncs["test"] = func(ent *TNE.Entity, world *TNE.World) {
 		if (*world.FrameCounter)%30 == 0 {
 			ent.Move(1, 30)
 		}
 	}
 	//uFncs["test_2"] = uFncs["test"]
-	
+
 	cf.SetUpdateFunction(uFncs)
-	
+
 	prepStart := time.Now()
 	cf.Prepare()
 	fmt.Println("Preparing took: ", time.Now().Sub(prepStart))
-	
+
 	getStart := time.Now()
 	c := cf.Get(1)
 	fmt.Println("Getting took: ", time.Now().Sub(getStart))
-	game.character = &TNE.Player{TNE.Race{*c}}
-	
-	game.world = TNE.GetWorld(0,0,float64(screenWidth),float64(screenHeight), 16, 9, 4,6, cf, &game.frame, "./res/Worlds/TestWorld1", "TestMap1", "./res/Worlds/TestWorld1/tiles", "./res/Worlds/TestWorld1/structObjs")
+	game.character = &TNE.Player{TNE.Race{Entity: *c}}
+
+	game.world = TNE.GetWorld(0, 0, float64(screenWidth), float64(screenHeight), 16, 9, 4, 6, cf, &game.frame, "./res/Worlds/TestWorld1", "TestMap1", "./res/Worlds/TestWorld1/tiles", "./res/Worlds/TestWorld1/structObjs")
 	game.world.AddPlayer(game.character)
 	err = game.world.SetActivePlayer(0)
 	GE.ShitImDying(err)
-	
+
 	chicken := cf.Get(0)
-	fmt.Println("Chicken: ",chicken.Print())
-	game.world.AddEntity(0,0,chicken)
-	
+	fmt.Println("Chicken: ", chicken.Print())
+	game.world.AddEntity(0, 0, chicken)
+
 	fmt.Println(game.world.Print())
-	
+
 	game.Init(nil)
 	StartGame(game)
 }
-
