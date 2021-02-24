fa700604381403dcd89a640bc582a008f9158cdc
Mortim-Portim <>
2021-02-24 16:31:17 +0100

    fett

--- TNE/attacks.go
+++ TNE/attacks.go
@@ -16,28 +16,27 @@ type Attack interface {
 	-> if w != nil the attack should modifiy other entities (health) that it hits
 	**/
 	Start(pl *Player, w *World, x, y float64)
-
 	/**
 	-> updates the attack
 	-> is called every frame
 	**/
 	Update(pl *Player, w *World)
-
 	/**
 	-> returns if the attack is finished and can be deleted
 	**/
 	IsFinished() bool
-
 	/**
 	-> encodes the attack as a byte slice
 	-> will be transfered to the server and other clients to reconstruct the attack
 	**/
 	ToBytes() []byte
+}
 
-	/**
-	-> should load the attack from bytes
-	-> will be followed by a call to Start(pl *Player, w *World, x, y float64)
-	**/
+/**
+-> should load the attack from bytes
+-> will be followed by a call to Start(pl *Player, w *World, x, y float64)
+**/
+func GetAttackFromBytes(bs []byte) (a Attack) {
 
-	FromBytes(bs []byte)
+	return
 }
