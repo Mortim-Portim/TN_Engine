b3981a6a4f1617dafc1e848c51c5d9a411064dae
Mortim-Portim <>
2021-03-05 14:35:27 +0100

    fett

--- main.go
+++ main.go
@@ -1,160 +1,59 @@
 package main
-func main() {}
-//import (
-//	"github.com/mortim-portim/GraphEng/GE"
-//	"github.com/mortim-portim/TN_Engine/TNE"
-//
-//	"fmt"
-//	"time"
-//
-//	"github.com/hajimehoshi/ebiten"
-//	"github.com/hajimehoshi/ebiten/ebitenutil"
-//)
-//
-//var (
-//	screenWidth  = 1600
-//	screenHeight = 900
-//	FPS          = 30
-//)
-//
-//func StartGame(g ebiten.Game) {
-//	ebiten.SetWindowSize(screenWidth, screenHeight)
-//	ebiten.SetWindowTitle("TN_Engine Test")
-//	ebiten.SetFullscreen(true)
-//	ebiten.SetVsyncEnabled(true)
-//	ebiten.SetRunnableOnUnfocused(true)
-//	ebiten.SetMaxTPS(FPS)
-//	err := ebiten.RunGame(g)
-//	defer GE.CloseLogFile()
-//	GE.ShitImDying(err)
-//}
-//
-//type TestGame struct {
-//	character *TNE.Player
-//	world     *TNE.World
-//	rec       *GE.Recorder
-//	frame     int
-//}
-//
-//func (g *TestGame) Init(screen *ebiten.Image) {}
-//
-//var timeTaken int64
-//
-//func (g *TestGame) Update(screen *ebiten.Image) error {
-//	startTime := time.Now()
-//	moving := false
-//	if ebiten.IsKeyPressed(ebiten.KeyA) {
-//		g.character.ChangeOrientation(0)
-//		if GE.IsKeyJustDown(ebiten.KeyA) {
-//			g.character.Entity.Move(1, FPS/6)
-//		}
-//		moving = true
-//	}
-//	if ebiten.IsKeyPressed(ebiten.KeyD) {
-//		g.character.ChangeOrientation(1)
-//		if GE.IsKeyJustDown(ebiten.KeyD) {
-//			g.character.Entity.Move(1, FPS/6)
-//		}
-//		moving = true
-//	}
-//	if ebiten.IsKeyPressed(ebiten.KeyW) {
-//		g.character.ChangeOrientation(2)
-//		if GE.IsKeyJustDown(ebiten.KeyW) {
-//			g.character.Entity.Move(1, FPS/6)
-//		}
-//		moving = true
-//	}
-//	if ebiten.IsKeyPressed(ebiten.KeyS) {
-//		g.character.ChangeOrientation(3)
-//		if GE.IsKeyJustDown(ebiten.KeyS) {
-//			g.character.Entity.Move(1, FPS/6)
-//		}
-//		moving = true
-//	}
-//	g.character.KeepMoving(moving)
-//
-//	//Update chunks
-//	g.world.UpdatePlayerChunks(g.world.Players)
-//
-//	//Update world
-//	g.world.UpdateActivePlayer()
-//	g.world.UpdateDrawables()
-//	g.world.UpdateWorldStructure()
-//	g.world.Draw(screen)
-//
-//	//Save video
-//	if ebiten.IsKeyPressed(ebiten.KeyC) && !g.rec.IsSaving() {
-//		g.rec.Save("./res/out")
-//	}
-//	g.rec.NextFrame(screen)
-//
-//	g.frame++
-//	timeTaken = time.Now().Sub(startTime).Milliseconds()
-//	msg := TNE.PrintPerformance(int(g.frame-1), int(timeTaken))
-//	ebitenutil.DebugPrint(screen, msg)
-//	GE.LogToFile(msg + "\n")
-//	fmt.Println(msg)
-//	return nil
-//}
-//func (g *TestGame) Layout(outsideWidth, outsideHeight int) (int, int) {
-//	return screenWidth, screenHeight
-//}
-//func main() {
-//	screenWidth, screenHeight = ebiten.ScreenSizeInFullscreen()
-//	GE.Init("")
-//	GE.SetLogFile("./res/log.txt")
-//	time.Sleep(time.Second * 2)
-//
-//	diceStart := time.Now()
-//	result := TNE.RollDice(100000, 20)
-//	fmt.Println("Rolling Dice took: ", time.Now().Sub(diceStart))
-//	fmt.Println(result)
-//
-//	game := &TestGame{nil, nil, GE.GetNewRecorder(FPS*5, 928, 522, FPS), 0}
-//
-//	cf, err := TNE.GetEntityFactory("./res/creatures/", &game.frame, 3)
-//	GE.ShitImDying(err)
-//
-//	/*Updater := func(ent *TNE.Entity, world *TNE.World) {
-//		fmt.Println("Updating")
-//		if (*world.FrameCounter)%30 == 0 {
-//			ent.Move(1, 15)
-//		}
-//		ent.KeepMoving(true)
-//	}*/
-//
-//	uFncs := make(map[string]TNE.EntityUpdater)
-//	uFncs["test"] = &TNE.EnupPassive{}
-//	//uFncs["test_2"] = uFncs["test"]
-//
-//	cf.SetUpdateFunctionMap(uFncs)
-//
-//	prepStart := time.Now()
-//	cf.Prepare()
-//	fmt.Println("Preparing took: ", time.Now().Sub(prepStart))
-//
-//	getStart := time.Now()
-//	c, err := cf.Get(1)
-//	if err != nil {fmt.Println(err)}
-//	fmt.Println("Getting took: ", time.Now().Sub(getStart))
-//	game.character = &TNE.Player{TNE.Race{Entity: *c}}
-//
-//	game.world = TNE.GetWorld(0, 0, float64(screenWidth), float64(screenHeight), 45, 27, 7, 7, 1, cf, &game.frame, "./res/Worlds/TestWorld1", "TestMap1", "./res/Worlds/TestWorld1/tiles", "./res/Worlds/TestWorld1/structObjs")
-//	game.world.AddPlayer(game.character)
-//	err = game.world.SetActivePlayer(0)
-//	GE.ShitImDying(err)
-//
-//	chicken, err := cf.Get(0)
-//	if err != nil {fmt.Println(err)}
-//	chicken.ChangeOrientation(1)
-//	fmt.Println("Chicken: ", chicken.Print())
-//	err = game.world.AddEntity(0, 0, chicken)
-//	GE.ShitImDying(err)
-//
-//	game.world.Structure.GetFrame(3.0, 100, 16)
-//
-//	fmt.Println(game.world.Print())
-//
-//	game.Init(nil)
-//	StartGame(game)
-//}
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/mortim-portim/GraphEng/GE"
+)
+
+var (
+	Width, Height float64
+	ScreenScale   = 32.0
+)
+
+func main() {
+	GE.Init("", 30)
+	x, y := ebiten.ScreenSizeInFullscreen()
+	Width, Height = float64(x), float64(y)
+	ps := GE.GetNewParticleSystem(10)
+	sprites, err := GE.LoadEbitenImg("./res/arrow.png")
+	GE.ShitImDying(err)
+	psAnim := GE.GetAnimation(0, 0, 1, 1, 16, 6, sprites)
+	pf := GE.GetNewParticleFactory(100, 30, psAnim)
+	//ps.Add(pf.GetNewRandom(0, 1.0, 50, 50, 1, 1))
+
+	game := &TestGame{ps, pf, 0}
+	ebiten.SetWindowSize(int(Width), int(Height))
+	ebiten.SetWindowTitle("Particles")
+	ebiten.SetMaxTPS(30)
+	ebiten.SetVsyncEnabled(true)
+	ebiten.SetRunnableOnUnfocused(true)
+	ebiten.SetScreenTransparent(true)
+	//ebiten.SetWindowFloating(true)
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type TestGame struct {
+	ps           *GE.ParticleSystem
+	pf           *GE.ParticleFactory
+	frameCounter int
+}
+
+func (g *TestGame) Update() error {
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		x, y := ebiten.CursorPosition()
+		g.ps.Add(g.pf.GetNewRandom(g.frameCounter, 1.0, float64(x)/ScreenScale, float64(y)/ScreenScale, 1, 1))
+	}
+	g.ps.Update(g.frameCounter)
+	g.frameCounter++
+	return nil
+}
+func (g *TestGame) Draw(screen *ebiten.Image) {
+	g.ps.DrawOnPlainScreen(screen, 0, 0, ScreenScale)
+}
+func (g *TestGame) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return int(Width), int(Height)
+}
